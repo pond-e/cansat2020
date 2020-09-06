9f3a32d2444981849da817e9aedc7443a391dc3a
puku2202 <m11834@s.akashi.ac.jp>
2020-09-06 22:00:48 +0900

    elmo

---
+++ test1/b.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+        cout << "helloworld" << endl;
+	return 0;
+}                                                                                                                                                     
+                                                                         

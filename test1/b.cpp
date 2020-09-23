1010c9add2ab9689fa851af32a2baa25df3e0620
puku2202 <m1834@s.akashi.ac.jp>
2020-09-23 11:40:13 +0900

    l

--- test1/b.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(void){
-        cout << "helloworld" << endl;
-	return 0;
-}                                                                                                                                                     
-                                                                         
